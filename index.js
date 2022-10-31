a6dbf2257954c0bdbde48c8a98009a09412b08be
sanyaswb <sanyaswb@gmail.com>
2022-10-31 15:41:32 +0200

    completed 05_get-distance

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getDistance(distanceTraveled, traveledPercent) {
-	// write your code here
+	return 'distance between village and capital: ' + String(distanceTraveled * (100 / traveledPercent)) + 'm';
 }
 
 module.exports = getDistance;
